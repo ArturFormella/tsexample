abcac266112b8196469c0cc58b3f01fda248fa0a
ArturFormella <ArturFormella@users.noreply.github.com>
2017-05-21 21:16:36 +0200

    Removed cutdict dictionary

--- tsexample--1.0.sql
+++ tsexample--1.0.sql
@@ -29,34 +29,13 @@ CREATE TEXT SEARCH PARSER sample_parser (
 	END = sparser_end,
 	LEXTYPES = sparser_lextype
 );
-COMMENT ON TEXT SEARCH PARSER sample_parser IS 'sample word parser';
-
-CREATE OR REPLACE FUNCTION cutdict_init(internal)
-	RETURNS internal
-	AS 'MODULE_PATHNAME'
-	LANGUAGE C STRICT IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION cutdict_lexize(internal, internal, internal, internal)
-	RETURNS internal
-	AS 'MODULE_PATHNAME'
-	LANGUAGE C STRICT IMMUTABLE;
-
-CREATE TEXT SEARCH TEMPLATE cutdict (
-	INIT = cutdict_init,
-	LEXIZE = cutdict_lexize
-);
-COMMENT ON TEXT SEARCH TEMPLATE cutdict IS 'cut dictionary: lowercase and keep only beginning and ending of long words';
-
-CREATE TEXT SEARCH DICTIONARY cut3 (
-	TEMPLATE = cutdict,
-	nbegin = 3,
-	nend = 3
-);
-COMMENT ON TEXT SEARCH DICTIONARY cut3 IS 'cut dictionary with nbegin = nend = 3';
+COMMENT ON TEXT SEARCH PARSER sample_parser IS 'whitespace separator';
 
 CREATE TEXT SEARCH CONFIGURATION sample (
 	PARSER = "sample_parser"
 );
-ALTER TEXT SEARCH CONFIGURATION sample ADD MAPPING FOR word WITH cut3;
+/*
+ALTER TEXT SEARCH CONFIGURATION sample ADD MAPPING FOR word WITH simple;
 ALTER TEXT SEARCH CONFIGURATION sample ADD MAPPING FOR number WITH simple;
+*/
 COMMENT ON TEXT SEARCH CONFIGURATION sample IS 'sample configuration';
